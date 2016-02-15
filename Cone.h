85d417ca0f53d7b22e9cdbe8b30e7299548cedd6
aberman <arthurberman94@gmail.com>
2016-02-15 14:32:12 -0500

    Start cone normals

--- Cone.h
+++ Cone.h
@@ -9,15 +9,28 @@ public:
 	Cone() {};
 	~Cone() {};
 
-    void drawCircle(float y){
+    void drawCircle(float y, bool normal){
         float r = 0.5f;
         for (int i = 0; i <= m_segmentsX; i++) {
             float theta = i * (2 * PI / m_segmentsX); 
             float x = r * cos(theta);
             float z = r * sin(theta);
-            
-            glVertex3f(x, y, z);
-            glVertex3f(0, y, 0);    
+            if (!normal) {
+                glNormal3f(0, -1, 0);
+                glVertex3f(x, y, z);
+                glNormal3f(0, -1, 0);
+                glVertex3f(0, y, 0);    
+            } else {
+                glBegin(GL_LINES);
+                glVertex3f(x, y, z);
+                glVertex3f(x, y - 0.1f, z);
+                glEnd();
+
+                glBegin(GL_LINES);
+                glVertex3f(0, y, 0);
+                glVertex3f(0, y - 0.1f, 0);
+                glEnd();
+            }
         } 
     };
 
@@ -42,7 +55,7 @@ public:
 
 	void draw() {
         glBegin(GL_TRIANGLE_STRIP);
-	    drawCircle(-0.5f);
+	    drawCircle(-0.5f, false);
         glEnd();
 
         glBegin(GL_TRIANGLE_STRIP);
@@ -51,6 +64,7 @@ public:
     };
 
 	void drawNormal() {
+	    drawCircle(-0.5f, true);
 	};
 };
 
